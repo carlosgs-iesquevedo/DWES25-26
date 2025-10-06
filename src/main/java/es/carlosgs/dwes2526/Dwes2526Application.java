ac137494f143daada242ba3211fd37d0db463cbe
CarlosGS <carlosgs.iesquevedo@gmail.com>
2025-10-06 11:34:40 +0200

    Inicio proyecto

---
+++ src/main/java/es/carlosgs/dwes2526/Dwes2526Application.java
@@ -0,0 +1,13 @@
+package es.carlosgs.dwes2526;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dwes2526Application {
+
+  static void main(String[] args) {
+    SpringApplication.run(Dwes2526Application.class, args);
+  }
+
+}
---
+++ src/test/java/es/carlosgs/dwes2526/Dwes2526ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.carlosgs.dwes2526;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dwes2526ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
