1f03cf01242d9b24f756029cde3478c59b3f5ab8
CarlosGS <carlosgs.iesquevedo@gmail.com>
2025-10-13 21:21:32 +0200

    tarjetas: Cache

--- src/main/java/es/carlosgs/dwes2526/Dwes2526Application.java
+++ src/main/java/es/carlosgs/dwes2526/Dwes2526Application.java
@@ -2,7 +2,9 @@ package es.carlosgs.dwes2526;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Dwes2526Application {
 
