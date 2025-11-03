2aa1e11ed550b95c5e76d4cd0f293d0dd6a61483
CarlosGS <carlosgs.iesquevedo@gmail.com>
2025-11-03 10:49:38 +0100

    tarjetas: CreditCardNumberValidator debe aceptar nulos

--- src/main/java/es/carlosgs/dwes2526/tarjetas/validators/CreditCardNumberValidator.java
+++ src/main/java/es/carlosgs/dwes2526/tarjetas/validators/CreditCardNumberValidator.java
@@ -14,12 +14,14 @@ public class CreditCardNumberValidator implements
   @Override
   public boolean isValid(String creditCardField,
                          ConstraintValidatorContext context) {
+    if (creditCardField == null) {
+      return true; // Permitir valores null, otras anotaciones controlan esto
+    }
     boolean valid16Digits = creditCardField.matches("[0-9]+")
         && creditCardField.length() == 16;
     boolean valid16WithSpaces = creditCardField.matches("([0-9]{4} ){3}[0-9]{4}");
     boolean valid16WithDashes = creditCardField.matches("([0-9]{4}-){3}[0-9]{4}");
-    return creditCardField != null &&
-        (valid16Digits || valid16WithSpaces || valid16WithDashes);
+    return valid16Digits || valid16WithSpaces || valid16WithDashes;
   }
 
 }
