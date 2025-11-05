7e69cb62a61ff24fdbd27f93f02327d8e69dbda7
CarlosGS <carlosgs.iesquevedo@gmail.com>
2025-11-05 13:36:41 +0100

    tarjetas_jpa: Tarjeta con @Entity

--- src/main/java/es/carlosgs/dwes2526/tarjetas/models/Tarjeta.java
+++ src/main/java/es/carlosgs/dwes2526/tarjetas/models/Tarjeta.java
@@ -1,24 +1,41 @@
 package es.carlosgs.dwes2526.tarjetas.models;
 
-import lombok.Builder;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Builder
-@Data
+@ToString
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor // JPA necesita un constructor vacío
+@Entity
+@Table(name = "TARJETAS")
 public class Tarjeta {
+  @Id // Indicamos que es el ID de la tabla
+  @GeneratedValue(strategy = GenerationType.IDENTITY) // Indicamos que es autoincremental y por el script de datos
   private Long id;
-
+  @Column(nullable = false, length = 19)
   private String numero;
+  @Column(nullable = false, length = 3)
   private String cvc;
   private LocalDate fechaCaducidad;
+  @Column(nullable = false, length = 50)
   private String titular;
+  @Column(nullable = false)
   private Double saldo;
-
+  @Column(updatable = false, nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
   private LocalDateTime createdAt;
+  @Column(nullable = false, columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
   private LocalDateTime updatedAt;
+  @Column(unique = true, updatable = false, nullable = false)
   private UUID uuid;
+
+  // nueva columna
+  @Column(columnDefinition = "boolean default false")
+  private Boolean isDeleted;
 }
