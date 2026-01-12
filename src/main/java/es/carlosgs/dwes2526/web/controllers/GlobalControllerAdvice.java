60d65371cf65e0a427258b624fe92c0cb4039856
CarlosGS <carlosgs.iesquevedo@gmail.com>
2026-01-12 16:14:06 +0100

    web: vista index

---
+++ src/main/java/es/carlosgs/dwes2526/web/controllers/GlobalControllerAdvice.java
@@ -0,0 +1,27 @@
+package es.carlosgs.dwes2526.web.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import java.time.LocalDate;
+
+// Aqui exponemos atributos globales para las vistas
+@ControllerAdvice
+public class GlobalControllerAdvice {
+
+  @Value("${spring.application.name}")
+  private String appName;
+
+  @ModelAttribute("appName")
+  public String getAppName() {
+    return appName;
+  }
+
+  @ModelAttribute("currentYear")
+  public int getCurrentYear() {
+    return LocalDate.now().getYear();
+  }
+
+
+}
---
+++ src/main/java/es/carlosgs/dwes2526/web/controllers/ZonaPublicaController.java
@@ -0,0 +1,36 @@
+package es.carlosgs.dwes2526.web.controllers;
+
+import es.carlosgs.dwes2526.rest.tarjetas.dto.TarjetaResponseDto;
+import es.carlosgs.dwes2526.rest.tarjetas.services.TarjetasService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Controller
+// Fijamos ya de por si una ruta por defecto para escuchar en este controlador
+@RequestMapping("/public")
+public class ZonaPublicaController {
+  private final TarjetasService tarjetasService;
+
+
+  @GetMapping({"", "/", "/index"})
+  public String index(Model model) {
+    Pageable pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
+    Page<TarjetaResponseDto> tarjetasPage = tarjetasService.findAll(
+        Optional.empty(), Optional.empty(), Optional.empty(), pageable);
+
+    model.addAttribute("tarjetas", tarjetasPage);
+    return "index";
+  }
+
+}
